ebbb674bf8d52010f2334b4c4214b72f17001231
M. Winston <239023611+0xWyn@users.noreply.github.com>
2026-05-22 17:44:21 +0000

    Wrote delete message controller

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -25,5 +25,30 @@ export const createMessage = async (req, res) => {
     }
 };
 export const getMessage = () => {};
-export const deleteMessage = () => {};
+
+export const deleteMessage = async (req, res) => {
+ try {
+    const {messageId } = req.params;
+    const user = req.user;
+    const message = Message.findById(messageId);
+    if (!message.sender === user._id.toString()) return res.status(402).json({error: "Failed to authenticate req.user"})
+        const chatId = message.chat;
+    const chat = Chat.findById(chatId);
+const conversation = Message.find({chat: chatId}).sort({createdAt: -1});
+if (chat.lastMessage.toString() === messageId) {
+    conversation.map((message, index) => {
+        if (!conversation[index + 1]) {
+            chat.lastMessage = message;
+            return;
+        }
+        return message;
+    })
+};
+await message.delete();
+return res.status(200).json({msg: "Message deleted successfully"})
+    } catch (error) {
+        res.status(500).json({error: error})
+    }
+};
+
 export const editMessage = () => {};
