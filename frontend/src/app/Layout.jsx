18ab113988c6b7ffa801787120a8a54299286d2e
0xWyn <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>
2026-06-14 00:44:07 +0100

    Fixing navigation bar

--- frontend/src/app/Layout.jsx
+++ frontend/src/app/Layout.jsx
@@ -3,7 +3,7 @@ import NavigationBar from "./NavigationBar";
 
 export default function Layout() {
     return (
-        <div className="md-flex-row flex flex-col flex-col-reverse w-screen min-w-sm h-screen items-stretch gap-2 p-2 bg-white overflow-x-scroll border">
+        <div className="md-flex-row flex w-screen min-w-sm h-screen items-stretch gap-2 p-2 bg-white overflow-x-scroll border">
             <NavigationBar />
             <div className="flex-1 min-w-0 w-full overflow-y-auto ">
                 <Outlet />
